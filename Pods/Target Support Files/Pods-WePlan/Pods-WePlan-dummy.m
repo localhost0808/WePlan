66aa41d1fb4a30e28dd0cb6e79a372111b813650
Harry <Localhost0808@gmail.com>
2018-08-14 22:38:32 +0800

    1

---
+++ Pods/Target Support Files/Pods-WePlan/Pods-WePlan-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WePlan : NSObject
+@end
+@implementation PodsDummy_Pods_WePlan
+@end
---
+++ Pods/Target Support Files/Pods-WePlanUITests/Pods-WePlanUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WePlanUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WePlanUITests
+@end
